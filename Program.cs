3dc231868dccdbea0501b3e293ae4d8a7d5804bd
DzhumagazievaDiana <113889448+DzhumagazievaDiana@users.noreply.github.com>
2022-09-27 00:08:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int a = 0;
+        Console.WriteLine("Введите a:");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Периметр нвадрата P={0}", a + a + a + a);
+        Console.ReadKey();
+    }
+}
